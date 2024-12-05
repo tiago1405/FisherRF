56abcca06018abed8dbc9ea4c0615c091ed9dcb8
Tiago da Costa <tiago.da.costa@nlr.nl>
2024-12-05 16:04:48 +0100

    Modified parsing to accept Namespace or Dict when initializing.

--- arguments/__init__.py
+++ arguments/__init__.py
@@ -9,6 +9,9 @@
 # For inquiries contact  george.drettakis@inria.fr
 #
 
+# Modified by Tiago Da Costa 02/12/2024 (dd/mm/yyyy)
+# To accept dict and argparser
+
 from argparse import ArgumentParser, Namespace
 import sys
 import os
@@ -17,35 +20,50 @@ class GroupParams:
     pass
 
 class ParamGroup:
-    def __init__(self, parser: ArgumentParser, name : str, fill_none = False):
-        group = parser.add_argument_group(name)
-        for key, value in vars(self).items():
-            shorthand = False
-            if key.startswith("_"):
-                shorthand = True
-                key = key[1:]
-            t = type(value)
-            value = value if not fill_none else None 
-            if shorthand:
-                if t == bool:
-                    group.add_argument("--" + key, ("-" + key[0:1]), default=value, action="store_true")
+    def __init__(self, parser_or_dict, name: str, fill_none=False):
+        if isinstance(parser_or_dict, dict):
+            for key, value in parser_or_dict.items():
+                if key.startswith("_"):
+                    key = key[1:]
+                setattr(self, key, value)
+        else:
+            group = parser_or_dict.add_argument_group(name)
+            for key, value in vars(self).items():
+                shorthand = False
+                if key.startswith("_"):
+                    shorthand = True
+                    key = key[1:]
+                t = type(value)
+                value = value if not fill_none else None 
+                if shorthand:
+                    if t == bool:
+                        group.add_argument("--" + key, ("-" + key[0:1]), default=value, action="store_true")
+                    else:
+                        group.add_argument("--" + key, ("-" + key[0:1]), default=value, type=t)
                 else:
-                    group.add_argument("--" + key, ("-" + key[0:1]), default=value, type=t)
-            else:
-                if t == bool:
-                    group.add_argument("--" + key, default=value, action="store_true")
-                else:
-                    group.add_argument("--" + key, default=value, type=t)
+                    if t == bool:
+                        group.add_argument("--" + key, default=value, action="store_true")
+                    else:
+                        group.add_argument("--" + key, default=value, type=t)
 
-    def extract(self, args):
+    def extract(self, args_or_dict):
+        # Modified extract just handles the '_' infront of some of these variables when extracting
         group = GroupParams()
-        for arg in vars(args).items():
-            if arg[0] in vars(self) or ("_" + arg[0]) in vars(self):
-                setattr(group, arg[0], arg[1])
+        if isinstance(args_or_dict, dict):
+            for key, value in vars(self).items():
+                stripped_key = key[1:] if key.startswith('_') else key
+                if stripped_key in args_or_dict:
+                    setattr(group, stripped_key, args_or_dict[stripped_key])
+                else:
+                    setattr(group, stripped_key, value)
+        else:
+            for arg in vars(args_or_dict).items():
+                if arg[0] in vars(self) or ("_" + arg[0]) in vars(self):
+                    setattr(group, arg[0], arg[1])
         return group
 
-class ModelParams(ParamGroup): 
-    def __init__(self, parser, sentinel=False):
+class ModelParams(ParamGroup):
+    def __init__(self, parser_or_dict, sentinel=False):
         self.sh_degree = 3
         self._source_path = ""
         self._model_path = ""
@@ -54,22 +72,23 @@ class ModelParams(ParamGroup):
         self._white_background = False
         self.data_device = "cuda"
         self.eval = False
-        super().__init__(parser, "Loading Parameters", sentinel)
+        super().__init__(parser_or_dict, "Loading Parameters", sentinel)
 
-    def extract(self, args):
-        g = super().extract(args)
-        g.source_path = os.path.abspath(g.source_path)
+    def extract(self, args_or_dict):
+        g = super().extract(args_or_dict)
+        if hasattr(g, 'source_path'):
+            g.source_path = os.path.abspath(g.source_path)
         return g
 
 class PipelineParams(ParamGroup):
-    def __init__(self, parser):
+    def __init__(self, parser_or_dict):
         self.convert_SHs_python = False
         self.compute_cov3D_python = False
         self.debug = False
-        super().__init__(parser, "Pipeline Parameters")
+        super().__init__(parser_or_dict, "Pipeline Parameters")
 
 class OptimizationParams(ParamGroup):
-    def __init__(self, parser):
+    def __init__(self, parser_or_dict):
         self.iterations = 30_000
         self.position_lr_init = 0.00016
         self.position_lr_final = 0.0000016
@@ -86,9 +105,9 @@ class OptimizationParams(ParamGroup):
         self.densify_from_iter = 500
         self.densify_until_iter = 10_000
         self.densify_grad_threshold = 0.0002
-        super().__init__(parser, "Optimization Parameters")
+        super().__init__(parser_or_dict, "Optimization Parameters")
 
-def get_combined_args(parser : ArgumentParser):
+def get_combined_args(parser: ArgumentParser):
     cmdlne_string = sys.argv[1:]
     cfgfile_string = "Namespace()"
     args_cmdline = parser.parse_args(cmdlne_string)
